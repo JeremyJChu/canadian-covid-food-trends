074f683fc57ec946796b499b8efde3d00d859dac
Jeremy Chu <jeremychuj@gmail.com>
2021-04-19 20:54:04 -0400

    Website commit

---
+++ content.js
@@ -0,0 +1,16 @@
+const CONTENT = [
+  {
+    left: 'Test',
+    right: 'Test2'
+  },
+  {
+    left: 'Test3',
+    right: 'Test4'
+  }
+];
+
+// CONTENT.map((content, index) => {
+//   const div = document.createElement('div');
+//   div.innerHTML = 'Test';
+//   document.getElementById('content').appendChild(div);
+// });
